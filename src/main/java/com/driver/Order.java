3b747a40b5c53099ffa1993c920b010df72e8095
Algani Uthej <69247162+DESMOND414@users.noreply.github.com>
2023-05-13 18:27:59 +0530

    changes

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+@Setter
 public class Order {
 
     private String id;
@@ -9,11 +17,41 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.deliveryTime = covertDeliveryTime(deliveryTime);
+        this.id = id;
+    }
+
+    public void setDeliveryTime(String deliveryTime) {
+        this.deliveryTime = covertDeliveryTime(deliveryTime);
+    }
+
+    public static int covertDeliveryTime(String deliveryTime) {
+        List<String> list = Arrays.asList(deliveryTime.split(":"));
+        int HH = Integer.parseInt(list.get(0));
+        int MM = Integer.parseInt(list.get(1));
+        return HH * 60 + MM;
     }
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {return deliveryTime;}
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public static String convertDeliveryTime(int deliveryTime) {
+        int HH = deliveryTime / 60;
+        int MM = deliveryTime % 60;
+        String hh = String.valueOf(HH);
+        String mm = String.valueOf(MM);
+        if (hh.length() == 1) {
+            hh = '0' + hh;
+        }
+        if (mm.length() == 1) {
+            mm = '0' + mm;
+        }
+        return hh + ":" + mm;
+    }
+
 }
